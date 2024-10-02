5e041a54a2e2a2ee00a55839cc15d49a36fa56ea
AJ Lehmann <la59021@mail.harpercollege.edu>
2024-10-02 16:41:25 -0500

    1 And 2 Done

--- 1_getters.cpp
+++ 1_getters.cpp
@@ -1,45 +1,48 @@
 #include <iostream>
-
 using namespace std;
 
-class Rectangle
-{
-private:
-  int width;
-  int height;
+class Rectangle {
+    private:
+        int width;
+        int height;
 
-public:
-  Rectangle(int w, int h) : width(w), height(h) {}
+    public:
+        Rectangle(int w, int h) : width(w), height(h) {}
 
-  int get_width()
-  {
-    return this->width;
-  }
+        int get_width() {
+            return this->width;
+        }
 
-  int get_height()
-  {
-    return this->height;
-  }
+        int get_height() {
+            return this->height;
+        }
 
-  int area()
-  {
-    return this->width * this->height;
-  }
+        int area() {
+            return this->width * this->height;
+        }
 };
 
-Rectangle scale(Rectangle rectangle, int factor)
-{
-  int width = rectangle.get_width() * factor;
-  int height = rectangle.get_height() * factor;
-  return Rectangle(width, height);
+Rectangle scale(Rectangle rectangle, int factor) {
+    int width = rectangle.get_width() * factor;
+    int height = rectangle.get_height() * factor;
+    return Rectangle(width, height);
 }
 
-int main()
-{
-  Rectangle r1(2, 3);
-  cout << "Area of Rectangle 1: " << r1.area() << endl;
+Rectangle swap(Rectangle rectangle) {
+    int temp = rectangle.get_width();
+    int width = rectangle.get_height();
+    int height = temp;
+    return Rectangle(width, height);
+}
 
-  Rectangle r2 = scale(r1, 2);
-  cout << "Area of Rectangle 2: " << r2.area() << endl;
-  return 0;
+int main() {
+    Rectangle r1(2, 3);
+    cout << "Area of Rectangle 1: " << r1.area() << endl;
+
+    Rectangle r2 = scale(r1, 2);
+    cout << "Area of Rectangle 2: " << r2.area() << endl;
+
+    Rectangle r3 = swap(r2);
+    cout << "Area of Rectangle 3: " << r3.area() << endl;
+    return 0;
 }
--- 2_friend_functions.cpp
+++ 2_friend_functions.cpp
@@ -1,37 +1,43 @@
 #include <iostream>
-
 using namespace std;
 
-class Rectangle
-{
-private:
-  int width;
-  int height;
+class Rectangle {
+    private:
+        int width;
+        int height;
 
-public:
-  Rectangle(int w, int h) : width(w), height(h) {}
+    public:
+        Rectangle(int w, int h) : width(w), height(h) {}
 
-  int area()
-  {
-    return this->width * this->height;
-  }
+        int area() {
+            return this->width * this->height;
+        }
 
-  friend Rectangle scale(Rectangle, int);
+        friend Rectangle scale(Rectangle, int);
+        friend Rectangle swap(Rectangle);
 };
 
-Rectangle scale(Rectangle rectangle, int factor)
-{
-  int new_width = rectangle.width * factor;
-  int new_height = rectangle.height * factor;
-  return Rectangle(new_width, new_height);
+Rectangle scale(Rectangle rectangle, int factor) {
+    int new_width = rectangle.width * factor;
+    int new_height = rectangle.height * factor;
+    return Rectangle(new_width, new_height);
 }
 
-int main()
-{
-  Rectangle r1(2, 3);
-  cout << "Area of Rectangle 1: " << r1.area() << endl;
+Rectangle swap(Rectangle rectangle) {
+    int temp = rectangle.width;
+    int new_width = rectangle.height;
+    int new_height = temp;
+    return Rectangle(new_width, new_height);
+}
 
-  Rectangle r2 = scale(r1, 2);
-  cout << "Area of Rectangle 2: " << r2.area() << endl;
+int main() {
+    Rectangle r1(2, 3);
+    cout << "Area of Rectangle 1: " << r1.area() << endl;
+
+    Rectangle r2 = scale(r1, 2);
+    cout << "Area of Rectangle 2: " << r2.area() << endl;
+
+    Rectangle r3 = swap(r2);
+    cout << "Area of Rectangle 3: " << r3.area() << endl;
   return 0;
 }
